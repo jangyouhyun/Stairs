6ecd5ae7658ebfc493c2a3b301b816847d9f23ec
jangyouhyun <youhyun43@gmail.com>
2024-07-21 18:07:07 +0900

    [240721] 프론트 수정

--- frontend/src/components/ImageContainer.js
+++ frontend/src/components/ImageContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './ImageContainer.css';
-import bookImage from '../assets/images/book1.png';
+import book1 from '../assets/images/book1.png';
+import book2 from '../assets/images/book2.png';
 
 function ImageContainer() {
   return (
     <div className="image-container">
-      <img src={bookImage} alt="Book 1" className="book-image" />
+      <img src={book1} alt="Book 1" className="book-image book-image-1" />
+      <img src={book2} alt="Book 2" className="book-image book-image-2" />
     </div>
   );
 }
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './MainPage.css';
+import chatbotImage from '../assets/images/chatbot1.png'; // 이미지 경로 수정
+import signupIcon from '../assets/images/signup-icon.png'; // signup-icon 이미지 임포트
 
 function MainPage() {
   return (
-    <div className="main-container">
+    <div className="main-page">
       <header className="main-header">
         <button className="menu-button">☰</button>
-        <div className="main-title">자서전에 들어갔으면 하는 내용을 적어주세요!</div>
-        <button className="profile-button">⚪</button>
+        <button className="profile-button">
+          <img src={signupIcon} alt="Profile" className="profile-image" />
+        </button>
       </header>
       <div className="main-content">
-        <img src="/path/to/your/image.png" alt="책과 전구 이미지" className="main-image" />
+        <div className="title-image-container">
+          <img src={chatbotImage} alt="Chatbot" className="main-image" />
+          <h1 className="main-title">자서전에 들어갔으면 하는 내용을 적어주세요!</h1>
+        </div>
         <button className="question-button">입력내용으로 질문받기...</button>
         <textarea className="main-textarea" placeholder="내 인생의 사건에 대해서 적어주세요..."></textarea>
       </div>
