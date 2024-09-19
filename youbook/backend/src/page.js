da5b9b8e8b67823436cdb9a4989243b19368d432
mminnjji <9336pure@naver.com>
2024-09-19 09:08:14 +0000

    [240919] ssh 서버 연결 관련 수정

--- youbook/backend/src/page.js
+++ youbook/backend/src/page.js
@@ -25,7 +25,7 @@ app.use(morgan('dev'));
 
 // 세션설정
 const redisClient = new Redis({
-    host: '127.0.0.1',  // Redis 서버 호스트
+    host: 'localhost',  // Redis 서버 호스트
     port: 6379         // Redis 서버 포트
 });
 
@@ -81,6 +81,6 @@ app.use((req, res, next) => {
 
 // 포트 연결
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
