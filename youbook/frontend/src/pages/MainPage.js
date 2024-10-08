9e8bc0ad79bfb61f2c56c5165b8d59c8f9d18c48
mminnjji <9336pure@naver.com>
2024-10-08 04:33:48 +0000

    [241008] 데베 접속오류 수정

--- youbook/frontend/src/pages/MainPage.js
+++ youbook/frontend/src/pages/MainPage.js
@@ -17,12 +17,16 @@ import logout2 from '../assets/images/log-out2.png';
 
 function MainPage() {
   const [userName, setUserName] = useState(''); // 사용자의 이름을 저장할 상태 변수
-  const [selectedCategory, setSelectedCategory] = useState('');
+  //const [selectedCategory, setSelectedCategory] = useState('');
   const [profileImagePath, setProfileImagePath] = useState(defaultProfileImage); // 프로필 이미지를 저장할 상태 변수
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
   const [text, setText] = useState('');
   const [items, setItems] = useState([]); // 빈 배열로 초기화
   const [isRectangleVisible, setIsRectangleVisible] = useState(false); 
+  const location = useLocation();
+  const selectedCategory = location.state?.selectedCategory;
+
+
   const handleInquiryClick = () => {
     setIsRectangleVisible(!isRectangleVisible);
   };
