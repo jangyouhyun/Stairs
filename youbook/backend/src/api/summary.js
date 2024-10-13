181e81297fd2e91e873e8a4530bdd9007a09dc8b
mminnjji <9336pure@naver.com>
2024-10-13 19:29:33 +0000

    [241013]이미지삽입잘되고달리도됨

--- youbook/backend/src/api/summary.js
+++ youbook/backend/src/api/summary.js
@@ -1,4 +1,4 @@
-var express = require('express');
+   var express = require('express');
 var router = express.Router();
 var db = require('../db.js');
 const { OpenAI } = require('openai');
--- youbook/frontend/src/pages/BookModifyPage.js
+++ youbook/frontend/src/pages/BookModifyPage.js
@@ -44,7 +44,7 @@ function BookModifyPage() {
   const { userId, bookId } = useParams();   // URL에서 bookId 추출
   const location = useLocation();
   const { paragraph, category: initialCategory, title, subtitle, imageUrl } = location.state || {};  // 전달된 데이터를 수신
-  const [category, setCategory] = useState(initialCategory || '');
+  const selectedCategory = location.state?.selectedCategory;
   const [contentArray, setContentArray] = useState([]); // 빈 배열로 초기화
   const [content, setContent] = useState({
     title: title || '',         // 제목 설정
@@ -62,9 +62,7 @@ function BookModifyPage() {
   const [bookContent, setBookContent] = useState([]);
   const [categories, setCategories] = useState([]);
   const bookRef = useRef(null);
- // const [selectedCategory, setSelectedCategory] = useState(''); // 선택된 카테고리
- const { newcontentArray, selectedCategory } = location.state || {};
-  // 카테고리 목록 가져오기
+
   const fetchCategories = async () => {
     try {
       const response = await fetch('/api/get_category', {
@@ -495,7 +493,7 @@ const handleDeleteClick = async () => {
         // 페이지 이동 시 데이터를 함께 전달
         navigate('/my-autobiography', {
           state: {
-            category,
+            selectedCategory,
             title: bookName,
             image: imgData,  // 저장된 이미지 데이터
           },
